5234cb368b870648b81bc4f4d2785b6fbc62697e
Serhii Chernenko <serhii.chernenko@vaimo.com>
2024-09-10 16:00:31 +0200

    linters added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,21 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier/skip-formatting',
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        'linebreak-style': ['error', 'unix'],
+        'no-console': 'warn',
+        'vue/multi-word-component-names': 'off',
+        'vue/no-multiple-template-root': 'off',
+        'vue/no-v-html': 'off',
+    },
 }
